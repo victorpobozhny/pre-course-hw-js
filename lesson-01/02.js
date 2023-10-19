8975df8174872b7b13e8a6e7829ab110769b5da6
victorpobozhny <pobozhny94@gmail.com>
2023-10-19 13:50:04 +0300

    test 2 - 4

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,2 @@
 let number1 = Math.floor(Math.random()*100)
-if (number1%2 ===0) {
-    console.log(number1+ `: Это четное число`)
-} else {
-    console.log(number1+ `: Это нечетное число`)
-}
+number1%2 === 0 ? console.log(`${number1}: Это четное число`) : console.log(`${number1}: Это нечетное число`)
