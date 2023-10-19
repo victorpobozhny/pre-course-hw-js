d02fa20cbbfcb44494b40231953ecda668909ff8
victorpobozhny <pobozhny94@gmail.com>
2023-10-19 16:46:23 +0300

    test 5 - 2

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,6 +1,5 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-
-switch (rule) {
+switch (true) {
     case rule.length < 25 : console.log(`Все таки нет правил без исключения`)
         break
     case rule.length > 25 : console.log(`У меня 100 пудов все получится`)
