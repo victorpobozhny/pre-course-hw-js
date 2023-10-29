996f6d951b8d53af600e10787f283da11997ad0a
victorpobozhny <pobozhny94@gmail.com>
2023-10-29 10:37:01 +0300

    test 3-1-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,5 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+function createColorString() {
+    return colors.filter(e => e==='черный' ||e==='красный'|| e==='желтый').join('-')
+}
+console.log(createColorString());
